adc57e33e13f519d082ffe3ff784ce39c47f3f0f
Felix <202006933@alu.comillas.edu>
2026-01-29 08:44:54 +0100

    clean

--- scripts/bootstrap-ngixpython.sh
+++ scripts/bootstrap-ngixpython.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# 1. Instalar solo lo necesario (sin update completo para ganar velocidad)
 yum install -y python3-pip nginx
 
-# 2. Instalar FastAPI y Uvicorn
 pip3 install fastapi uvicorn
 
-# 3. Crear la App en Python
 mkdir -p /home/ec2-user/app
 cat << 'EOF' > /home/ec2-user/app/main.py
 from fastapi import FastAPI
@@ -23,8 +20,6 @@ def read_root():
     }
 EOF
 
-# 4. --- FIX IMPORTANTE: Crear servicio Systemd ---
-# Esto garantiza que Python arranque como un servicio real y no muera
 cat << 'EOF' > /etc/systemd/system/fastapi.service
 [Unit]
 Description=FastAPI App
@@ -40,12 +35,10 @@ Restart=always
 WantedBy=multi-user.target
 EOF
 
-# Recargar demonios y arrancar Python
 systemctl daemon-reload
 systemctl start fastapi
 systemctl enable fastapi
 
-# 5. Configurar NGINX como Reverse Proxy
 cat << 'EOF' > /etc/nginx/conf.d/fastapi.conf
 server {
     listen 80;
@@ -59,7 +52,6 @@ server {
 }
 EOF
 
-# Ajuste de config por defecto
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
 cat << 'EOF' > /etc/nginx/nginx.conf
 user nginx;
@@ -78,10 +70,7 @@ http {
 }
 EOF
 
-# 6. --- FIX CRÍTICO: SELinux ---
-# Permitir que NGINX hable con la red (puerto 8000)
 setsebool -P httpd_can_network_connect 1
 
-# 7. Arrancar NGINX
 systemctl start nginx
 systemctl enable nginx
